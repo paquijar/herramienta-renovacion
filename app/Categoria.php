1c2aa275b0e14ee01ef0a5591b56026f0e40bcc3
Pablo Quijano <pablo.quijanoj@perficient.com>
2021-05-01 18:52:24 -0500

    subcategoria mayuscula

--- app/Categoria.php
+++ app/Categoria.php
@@ -4,14 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Categoria extends Model {
-	public $timestamps = false;
-	protected $fillable = [
-		'nombre', 'peso',
-	];
+class Categoria extends Model
+{
+    public $timestamps = false;
+    protected $fillable = [
+        'nombre', 'peso',
+    ];
 
-	public function subcategorias() {
-		return $this
-			->hasMany('App\subcategoria');
-	}
+    public function subcategorias()
+    {
+        return $this
+            ->hasMany('App\Subcategoria');
+    }
 }
